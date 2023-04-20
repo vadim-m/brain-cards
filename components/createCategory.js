fb267117e85c380e68b3ea41fa6f14a90fe2c975
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-04-20 13:46:46 +0300

    feat: done render categoryItem

--- components/createCategory.js
+++ components/createCategory.js
@@ -18,9 +18,37 @@ export const createCategory = (app) => {
   const createCategoryItem = (data) => {
     const item = createElement("li", {
       className: "category__item",
+    });
+
+    item.dataset.id = data.id;
+
+    const btnCard = createElement("button", {
+      className: "category__card",
+    });
+
+    const titleText = createElement("span", {
+      className: "category__title",
       textContent: data.title,
     });
-    item.dataset.id = data.id;
+
+    const countPairs = createElement("span", {
+      className: "category__pairs",
+      textContent: `${data.length} пар`,
+    });
+
+    btnCard.append(titleText, countPairs);
+
+    const btnEdit = createElement("button", {
+      className: "category__btn category__edit",
+      ariaLabel: "редактировать",
+    });
+
+    const btnDel = createElement("button", {
+      className: "category__btn category__del",
+      ariaLabel: "удалить",
+    });
+
+    item.append(btnCard, btnEdit, btnDel);
 
     return item;
   };
